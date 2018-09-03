928466de353c7497c43a23f620326aa925f66579
Francçois Lafitte <FranciscoII@Bionuketeks.home>
2018-09-03 20:34:16 +0200

    methodes ajoutées

--- src/Point.java
+++ src/Point.java
@@ -11,19 +11,19 @@ public class Point{
 	public int getZ() {return z;}
 	public void setZ(int z) {this.z = z;}
 	
-	//Constructor
+	//Constructors
+	Point(int x, int y, int z)  {
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
 	Point(int x, int y)  {
 		this.x=x;
 		this.y=y;
+		this.z=0;
 	}
 
-	// Move Pixel
-	public void move(int x, int y){
-		this.x=this.x+x;
-		this.y=this.y+y;
-		this.z=this.z+z;
-	}
-	
+
 	public String toString() {return "X:" + x + ", Y:" + y + ", Z:" + z;}
 }
 
