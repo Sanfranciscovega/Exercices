86e6377914bdcb5b4f07e8661692343b5ddbd57e
Francçois Lafitte <FranciscoII@Bionuketeks.home>
2018-09-03 20:34:16 +0200

    methodes ajoutées

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,16 @@ import java.awt.Color;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		Pixel p=new Pixel(12255, 25, Color.red);
+		Pixel p=new Pixel(1055, 25, Color.red);
 		infos(p);
 		// Move Pixel
-		p.move(15, 10);
+		p.move(100, 10);
 		infos(p);
 		// Change color
 		p.setColor(Color.BLUE);
 		infos(p);
 		
+
 	}
 
 	private static void infos(Pixel p) {
--- src/Pixel.java
+++ src/Pixel.java
@@ -1,34 +1,58 @@
 import java.awt.Color;
+
 	
 public class Pixel extends Point{
 	private Color color;
 	static final int MAX_X=2560;
 	static final int MAX_Y=1440;
+	private int oldX;
+	private int oldY;
 	
 	// Getter and setter
 	public Color getColor() {return color;}
 	public void setColor(Color color) {this.color = color;}
 	
-	Pixel(int x, int y) throws Exception {
+	Pixel(int x, int y)  {
 		// Initialize super class constructor
 		// Must be the first method
 		super(x, y);
-		if (x<0  || x>MAX_X || y<0  || y>MAX_Y)
-			throw new Exception ("Pixel hors bornes");
+		isInBound();
+		storeOldPositions();
 	}
 	
-	Pixel(int x, int y, Color color) throws Exception  {
+	Pixel(int x, int y, Color color)   {
 		// Initialize super class constructor
 		// Must be the first method
 		super(x, y);
-		if (x<0  || x>MAX_X || y<0  || y>MAX_Y)
-			throw new Exception ("Pixel hors bornes");
+		isInBound();
+		storeOldPositions();
 		this.color=color;
 	}
 	
+	// Move Pixel
+	public void move(int x, int y){
+		// TODO : check before assigning values
+		storeOldPositions();
+		super.x=super.x+x;
+		super.y=super.y+y;
+		isInBound();
+	}
+	
+	private void storeOldPositions() {
+		oldX=super.x;
+		oldY=super.y;
+}
+
 	
 	//Overload method toString of super class Point
-	public String toString() {return (super.toString() + ", couleur :" + color.toString());
+	public String toString() {return (super.toString() + ", couleur :" + color.toString());}
+	
+	private void isInBound() {
+		if (x<0  || x>MAX_X || y<0  || y>MAX_Y)	{outOfBound();}
+	}
+	
+	private void outOfBound() {
+		throw new Error ("Pixel hors bornes / X: " + super.x + " (max:" + MAX_X + "), Y: " + super.y + " (max:" + MAX_Y +")");
 	}
 }
 
